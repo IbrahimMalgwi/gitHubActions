e3a2084200ea228cba20e25876dfea296f860be4
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-03 08:28:18 +0100

    Create github-action-script.sh
    script

---
+++ github-action-script.sh
@@ -0,0 +1,28 @@
+          echo whats is in this directory?
+          ls -a
+          echo
+          echo ls Java Installed?
+          java --version
+          echo
+          echo is Git installed?
+          git --version
+          echo what about web tools?
+          mvn --version
+          gradle --version
+          ant -version
+          echo
+          echo where is the android SDK root?
+          echo $ANDROID_SDK_ROOT
+          echo
+          echo where is the Selenium jars?
+          echo $SELENIUM_JAR_PATH
+          echo
+          echo what is the workspace location?
+          echo $RUNNER_WORKSPACE
+          echo
+          echo who is runnning te script?
+          whoami
+          echo how is the disk layout?
+          df
+          echo what environment variables are available?
+          env
